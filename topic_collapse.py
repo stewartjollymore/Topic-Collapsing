6fde71015e05bebc3fdf19fdf3ae87e764ba9ae0
Stewart Jollymore <101573950+stewartjollymore@users.noreply.github.com>
2025-02-06 15:22:50 -0500

    Update topic_collapse.py
    adding missing functions

--- topic_collapse.py
+++ topic_collapse.py
@@ -1,4 +1,37 @@
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+def c_tf_idf(documents, m, ngram_range = (1,2)):
+ 
+  count = CountVectorizer(ngram_range = ngram_range, stop_words = "english").fit(documents)
+  t = count.transform(documents).toarray()
+  w = t.sum(axis = 1)
+  tf = np.divide(t.T, w)
+  sum_t = t.sum(axis = 0)
+  idf = np.log(np.divide(m, sum_t)).reshape(-1,1)
+  tf_idf = np.multiply(tf, idf)
+ 
+  return tf_idf, count
+
+def extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20):
+    words = count.get_feature_names()
+    labels = list(docs_per_topic.Topic)
+    tf_idf_transposed = tf_idf.T
+    indices = tf_idf_transposed.argsort()[:, -n:]
+    top_n_words = {label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
+    return top_n_words
+
+def extract_topic_sizes(df):
+    topic_sizes = (df.groupby(['Topic'])
+                     .Doc
+                     .count()
+                     .reset_index()
+                     .rename({"Topic": "Topic", "Doc": "Size"}, axis='columns')
+                     .sort_values("Size", ascending=False))
+    return topic_sizes
 
 # Create similarity matrix from a c-tf-idf matrix
 similarities_test = cosine_similarity(tf_idf.T)
